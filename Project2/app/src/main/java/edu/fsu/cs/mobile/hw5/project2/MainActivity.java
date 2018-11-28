9b987f9d73fb520294046f945e5b587a7c625bb3
camirios <cr15h@my.fsu.edu>
2018-11-27 21:41:35 -0500

    Added firebase/store login
    added

--- Project2/app/src/main/java/edu/fsu/cs/mobile/hw5/project2/MainActivity.java
+++ Project2/app/src/main/java/edu/fsu/cs/mobile/hw5/project2/MainActivity.java
@@ -1,13 +1,84 @@
 package edu.fsu.cs.mobile.hw5.project2;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private Map<String, Object> user=new HashMap<>();
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db=FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
-}
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                user.put("Name", currentUser.getDisplayName());
+                db.collection("Users").document(currentUser.getUid()).update(user)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                //add this in with navActivity
+                                //Intent myIntent= new Intent(MainActivity.this, NavActivity.class);
+                               // startActivity(myIntent);
+                            }
+                        });
+
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }//end of onActivityResult
+
+
+
+}//end of Main
