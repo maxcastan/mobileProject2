00ed1392114e9cebbdecb010fc6648d66ed89485
Emilio Sillart <esillart314@gmail.com>
2018-12-05 22:01:39 -0500

    Added RequestFragment, User class, UserAdapter
    adds the above plus a user_card.xml.  Currently there are some errors in UserAdapter class

---
+++ Project2/app/src/main/java/edu/fsu/cs/mobile/hw5/project2/UserAdapter.java
@@ -0,0 +1,69 @@
+package edu.fsu.cs.mobile.hw5.project2;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class UserAdapter extends FirestoreRecyclerAdapter<User, UserAdapter.userHolder> {
+
+    private OnItemClickListener listener;
+
+    public UserAdapter(@NonNull FirestoreRecyclerOptions<User> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull UserHolder holder, int position, @NonNull User model) {
+        holder.username.setText(model.getUserName());
+        holder.housename.setText(model.getHouseName());
+    }
+
+    /*              NOT SURE WHAT TO REPLACE R.LAYOUT.MESSAGE_CARD WITH
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.message_card,
+                viewGroup, false);
+        return new UserHolder(v);
+    }
+    */
+
+
+    class UserHolder extends RecyclerView.ViewHolder {
+        TextView username;//number
+        TextView housename; //message
+
+        public UserHolder(View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.text_title);
+            housename = itemView.findViewById(R.id.text_description);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION//protects from crash getting -1 position of a deleted item, -1 is dne
+                            && listener != null) {
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+        }
+
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+}
